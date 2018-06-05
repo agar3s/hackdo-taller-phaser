48f5eab42bae999f00019d1da7853b91743118cb
Giovanny Beltran <ekeisco@gmail.com>
2018-06-04 21:55:22 -0500

    build production

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,24 +1,18 @@
 const webpack = require('webpack')
-const path = require('path')
 const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
 const MinifyPlugin = require('babel-minify-webpack-plugin')
-
-// Phaser webpack config
-const phaserModule = path.join(__dirname, '/node_modules/phaser/')
-const phaser = path.join(phaserModule, 'src/phaser.js')
-
-const phaserPlugin = new webpack.DefinePlugin({
-  __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'false'))
-})
+var CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = merge(common, {
   mode: 'production',
   plugins: [
-    new MinifyPlugin(),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production')
     }),
-    phaserPlugin
+    new MinifyPlugin(),
+    new CopyWebpackPlugin([
+      { from: 'assets', to: 'assets' }
+    ])
   ]
 })
